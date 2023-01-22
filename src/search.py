adcc8ec1ded83862a1d00a6cfa8a357677e0923f
Sebastian Spicker <sebastian.spicker@googlemail.com>
2023-01-22 21:16:00 +0100

    feat(search): add keyword and sender/date filtering to the CLI

---
+++ src/search.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+
+def build_search_summary() -> dict[str, str]:
+    return {"scope": "search", "status": "ready"}
+
+# current lane: search
+def search_task() -> dict[str, str]:
+    return {"scope": "search", "status": "ready"}
