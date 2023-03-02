2301a4449d9b719e2d67619b9bf70b826270de7a
Sebastian Spicker <sebastian.spicker@googlemail.com>
2023-03-02 19:14:00 +0100

    feat(threads): reconstruct conversation threads from message ids

---
+++ src/threads.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+
+def build_threads_summary() -> dict[str, str]:
+    return {"scope": "threads", "status": "ready"}
+
+# current lane: threads
+def threads_task() -> dict[str, str]:
+    return {"scope": "threads", "status": "ready"}
