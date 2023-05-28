6e705bf5d56e6f7282b809163c74ce5b3b800c41
Sebastian Spicker <sebastian.spicker@googlemail.com>
2023-05-28 21:56:00 +0200

    test(threads): check the initial threads behavior

---
+++ tests/test_threads.py
@@ -0,0 +1,10 @@
+from __future__ import annotations
+
+def test_threads_smoke() -> None:
+    payload = {"scope": "threads"}
+    assert payload["scope"] == "threads"
+
+# regression note: threads
+def test_threads_regression() -> None:
+    payload = {"scope": "threads", "result": "ok"}
+    assert payload["result"] == "ok"
