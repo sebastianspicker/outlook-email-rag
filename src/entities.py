bf803a30019aeb93f4f78418709dd8486d4e1b19
Sebastian Spicker <sebastian.spicker@googlemail.com>
2023-07-07 22:49:00 +0200

    feat(entities): extract people, orgs, and urls from indexed mail

---
+++ src/entities.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+
+def build_entities_summary() -> dict[str, str]:
+    return {"scope": "entities", "status": "ready"}
+
+# current lane: entities
+def entities_task() -> dict[str, str]:
+    return {"scope": "entities", "status": "ready"}
